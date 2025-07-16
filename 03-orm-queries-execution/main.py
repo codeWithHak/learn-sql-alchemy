6d59b61b41b71c1c2cbd7f7a13a02962daea692d
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-07-16 15:28:43 +0500

    update queries

--- 03-orm-queries-execution/main.py
+++ 03-orm-queries-execution/main.py
@@ -24,9 +24,11 @@ insert_query = students.insert().values(id=3, name="Farrukh",major="BBA")
 
 delete_query = students.delete().where(students.c.id == 3)
 
+# slects all the tables if no where clause given you van give a where clause like given in delte_qquery
 select_query = students.select()
 
 update_query = students.update().where(students.c.id == 1).values(name="Khizar bakhtiyar")
+
 
 # execute query
 result = connection.execute(select_query)
@@ -34,8 +36,8 @@ result = connection.execute(select_query)
 # save data in db
 connection.commit()
 
+# fetch all tables that are selected
 for column in result.fetchall():
-    # print(f"Column: {str(column)}")
     
     print(f"\nID: {column[0]}")
     print(f"Name: {column[1]}")
